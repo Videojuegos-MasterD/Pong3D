fe93080fbf1310c1b6aacb26cb197dfe3ecea54c
Marsias9 <100529722+Marsias9@users.noreply.github.com>
2022-06-06 21:05:42 +0200

    v1.0.0
    -Countdown dismiss because of bugs.
    -Added a fade in and fade out between scenes.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
@@ -13,10 +14,10 @@ public class Ball : MonoBehaviour
     
     private SoundManager soundManager;
 
-    [SerializeField] public Image countdownImage;
+    //[SerializeField] public Image countdownImage;
+    //[SerializeField] public Sprite[] countdownSprite;
     [SerializeField] public Image winImage;
     [SerializeField] public Sprite[] winSprite;
-    [SerializeField] public Sprite[] countdownSprite;
     [SerializeField] private Animator myAnimationController;
 
     private void Awake()
@@ -46,7 +47,7 @@ public class Ball : MonoBehaviour
             soundManager.SelectionAudio(2, 0.25f);
             winImage.enabled = true;
             winImage.sprite = winSprite[winSprite.Length - 1];
-            Time.timeScale = 0;
+            Time.timeScale = 0; 
         }
         else
         {
@@ -59,23 +60,23 @@ public class Ball : MonoBehaviour
     {   //Velocidad y ángulo random.
         int velX = Random.Range(1, 3) == 1 ? Random.Range(-5, -5) : Random.Range(5, 5);
         int velZ = Random.Range(1, 3) == 1 ? Random.Range(-5, -5) : Random.Range(5, 5);
-
+        
         rb.velocity = new Vector3(0, 0, 0);
         transform.position = new Vector3(0, 0, 0);
     
-        countdownImage.sprite = countdownSprite[0];
+        /*countdownImage.sprite = countdownSprite[0];
 
-        Time.timeScale = 1f;
-
-        myAnimationController.SetTrigger("OnRestart");
+        Time.timeScale = 0f;
 
         for (int i = 3; i > 0; i--)//Cuenta atrás: 3... 2... 1... y la bola sale.
         {
-            /*while (PauseMenu.GameIsPaused)
+            myAnimationController.SetTrigger("OnRestart");
+
+            while (PauseMenu.GameIsPaused)
              {
                  yield return null;
-             }*/
-            
+             }
+
             countdownImage.enabled = true;
             countdownImage.sprite = countdownSprite[(i - 1) % countdownSprite.Length]; 
             yield return new WaitForSecondsRealtime(1);
@@ -84,7 +85,7 @@ public class Ball : MonoBehaviour
 
         countdownImage.enabled = false;
 
-        Time.timeScale = 1f;
+        Time.timeScale = 1f;*/
 
         rb.velocity = new Vector3(velX, 0, velZ);
 
@@ -102,6 +103,7 @@ public class Ball : MonoBehaviour
         }
 
         Wall wall = collision.collider.GetComponent<Wall>();
+
         if (wall)
         {
             soundManager.SelectionAudio(0, 0.25f);
